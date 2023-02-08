4a98044de6c4fcbae52311820c28a60bab3a3643
Anerikaria <karia.aneri@gmail.com>
2023-02-08 23:54:55 +1100

    Signed-off-by: Anerikaria <karia.aneri@gmail.com>

---
+++ JS-files/nav.js
@@ -0,0 +1,7 @@
+'use strict';
+
+// All selectors related to navigation goes here...
+const nav = document.querySelector('.nav');
+const stickyNav = document.querySelector('.sticky');
+
+// functions and methods goes here...
